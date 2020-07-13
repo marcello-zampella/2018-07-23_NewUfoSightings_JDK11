d609f38e821ceea2f5f4d7ec0227946166d56766
Utente <Utente@DESKTOP-73HFT01>
2020-07-13 19:42:44 +0200

    boh

--- src/main/java/it/polito/tdp/newufosightings/model/TestModel.java
+++ src/main/java/it/polito/tdp/newufosightings/model/TestModel.java
@@ -1,11 +1,19 @@
 package it.polito.tdp.newufosightings.model;
 
+import java.util.Random;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
 public class TestModel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Model m = new Model();
+		
+		Random rand=new Random();
+		for(int i=0;i<100;i++) {
+			System.out.println(rand.nextInt(51+1-50)+50);
+		}
+		
 	}
 
 }
